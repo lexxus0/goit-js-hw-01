96e931006d5109bdebe7f251a325d4a4d6f53a8e
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2024-05-19 19:37:54 +0200

    tasks done

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+'use strict';
+function getElementWidth(content, padding, border){
+    const contentNumber = Number.parseFloat(content);
+    const paddingNumber = Number.parseFloat(padding);
+    const borderNumber = Number.parseFloat(border);
+    return contentNumber + (paddingNumber * 2) + (borderNumber * 2) 
+}
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
